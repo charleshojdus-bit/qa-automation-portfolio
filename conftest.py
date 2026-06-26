938dc957b25c7fffe7734f14023b24f36bec9d75
Charles Hojdus <charles.hojdus@gmail.com>
2026-06-26 18:03:59 -0400

    Add GitHub Actions test workflow

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -6,7 +7,14 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--start-maximized")
+
+    if os.getenv("CI") == "true":
+        options.add_argument("--headless=new")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+    else:
+        options.add_argument("--start-maximized")
 
     options.add_experimental_option("prefs", {
         "credentials_enable_service": False,
